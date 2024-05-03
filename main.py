762e37d0ba2c9cdaaac34be2a85366f772c265e0
YellowClub <hello@yellowclub.by>
2024-05-03 18:53:56 +0300

    Lesson 1 - эхо бот создан

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-print ("первый проект леши няшки на пайтоне")
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+
+TOKEN = "7125759779:AAFJ8kSj09MrCSwlqRy_zyNtSwqObRiMNbA"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
 
 
+@dp.message(CommandStart())
+async def start(message: types.Message):
+    await message.answer("ЗДРАСТВУЙТЕ ПЕРЕД ВАМИ БОТ МОЛО КО КО КО СОЗДАНЫЙ ЛЁШЕЙ НЯШКОЙ")
 
 
+@dp.message()
+async def echo(message: types.Message):
+    # await message.answer("бот в разработке")
+    user_text = message.text
+    await message.answer(message.text)
 
+
+async def main():
+    print("бот запущен")
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
